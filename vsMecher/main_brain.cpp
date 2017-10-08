fbeb4f11dcdfcaced8797754fb261fc7104a67f0
MasayaNishijima <{masaya20000607@gmail.com}>
2017-10-08 23:41:28 +0900

    開発環境移行toVS
    VSに開発環境を移行並びにDXライブラリーの初期設定

---
+++ vsMecher/main_brain.cpp
@@ -0,0 +1,34 @@
+#include "standardInclude.h"
+#include "BouyomiChan.h"
+#include "quickAccessToWeb.h"
+
+
+
+
+int main(int argc, char const *argv[]) {
+
+
+  readWordIn = "何か御用でしょうか？";
+  SPEAK_MASTER;
+
+  while (TURE) {
+
+      cout << endl << "0.原初に戻る" << endl;
+      cout << "1.検索" <<endl;
+
+    SELECT_NUNBER;
+    if(selectNunber==0)break;
+      switch (selectNunber) {
+        case 1:
+          readWordIn= "検索を行います。";
+          SPEAK_MASTER;
+          quickAccessToWeb();
+
+    }
+    readWordIn = "他に御用はありますか？";
+    SPEAK_MASTER;
+  }
+  readWordIn = "了解です、お疲れさまでした！";
+  SPEAK_MASTER;
+  return 0;
+}
---
+++ vsMecher/standardInclude.h
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>//•W€“üo—Í
+using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#define SELECT_NUNBER cin >> selectNunber
+#define SPEAK_MASTER speakMasater()
+#define TURE 1
+#define FALSE 0
+static int selectNunber;
+static char *readWordIn, *readWordOut[2];
