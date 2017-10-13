89d3d97c743a95bbc94cfb992e622c093aa26a81
MasayaNishijima <{masaya20000607@gmail.com}>
2017-10-13 20:42:44 +0900

    天気予報更新(半端状態)

--- weatherAccess.h
+++ weatherAccess.h
@@ -1,11 +1,16 @@
+int weatherAccess(int num);
+
+int weatherAccessMaster(void){
+}
 
 
-int weatherAccess(void)
+
+
+int weatherAccess(int num)
 {
 	FILE *fp;	/* (1)ファイルポインタの宣言 */
 	char textNumber[48][256] = {0};
 	int gyo = 0;
-	int num;
 	char weather[64] = {"https://weather.yahoo.co.jp/weather/jp/"};
   char str[128];
   //strcpy(weather, "https://weather.yahoo.co.jp/weather/jp/");
@@ -21,18 +26,12 @@ int weatherAccess(void)
 	}
 
 
-	printf("都道府県番号を入力してください\n");
-	scanf("%d",&gyo);
-	printf("%s\n",textNumber[gyo]);
 	num = ((int)textNumber[gyo][15] - '0')*10 + ((int)textNumber[gyo][16] - '0');
-  printf("%d\n",num );
-	printf("%s%d/\n",weather,num);
-	if(gyo == 1){
+  if(gyo == 1){
 		ShellExecute(NULL, "open","https://weather.yahoo.co.jp/weather/jp/1.html?day=1", NULL, NULL, SW_SHOW);
 		return 0;
 	}
   sprintf(str,"https://weather.yahoo.co.jp/weather/jp/%d/",num);
-	printf("%s\n",str);
 	fclose(fp);	/* (5)ファイルのクローズ */
 	ShellExecute(NULL, "open", str , NULL, NULL, SW_SHOW);
 	//strcat(weather, (char)num);
