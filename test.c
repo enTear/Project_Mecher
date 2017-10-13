f3b99bc143271c26f8940dd3fc425021da9b9a9a
reiten10409 <30900592+reiten10409@users.noreply.github.com>
2017-10-13 18:30:06 +0900

    天気予報初期コード追加

---
+++ test.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <shellapi.h>
+#include <objbase.h>
+#include <shellapi.h>
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "ole32.lib")
+#pragma comment(lib, "shell32.lib")
+
+int main(void)
+{
+	FILE *fp;	/* (1)ファイルポインタの宣言 */
+	char textNumber[48][256] = {0};
+	int gyo = 0;
+	int num;
+	char weather[64] = {"https://weather.yahoo.co.jp/weather/jp/"};
+  char str[128];
+  //strcpy(weather, "https://weather.yahoo.co.jp/weather/jp/");
+  //char amari = '/';
+	/* (2)ファイルのオープン */
+	/*  ここで、ファイルポインタを取得する */
+	if ((fp = fopen("smpl.txt", "r")) == NULL) {
+		printf("file open error!!\n");
+		exit(EXIT_FAILURE);	/* (3)エラーの場合は通常、異常終了する */
+	}
+
+	for (int i=1;fgets(textNumber[i], 256, fp) != NULL;i++){
+	}
+
+
+	printf("行を入力してください\n");
+	scanf("%d",&gyo);
+	printf("%s\n",textNumber[gyo]);
+	num = ((int)textNumber[gyo][15] - '0')*10 + ((int)textNumber[gyo][16] - '0');
+  printf("%d\n",num );
+	printf("%s%d/\n",weather,num);
+	if(gyo == 1){
+		ShellExecute(NULL, "open","https://weather.yahoo.co.jp/weather/jp/1.html?day=1", NULL, NULL, SW_SHOW);
+		return 0;
+	}
+  sprintf(str,"https://weather.yahoo.co.jp/weather/jp/%d/",num);
+	printf("%s\n",str);
+	fclose(fp);	/* (5)ファイルのクローズ */
+	ShellExecute(NULL, "open", str , NULL, NULL, SW_SHOW);
+	//strcat(weather, (char)num);
+	//strcat(weather, amari);
+  //printf("%s\n",weather);
+
+
+	return 0;
+}
